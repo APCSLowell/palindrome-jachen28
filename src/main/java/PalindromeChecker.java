928458aea220597e1dc49c2553f197db351bfe8a
jachen28 <142927320+jachen28@users.noreply.github.com>
2024-01-29 09:36:59 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -38,7 +38,7 @@ public void tester()
 public boolean palindrome(String word)
 {
   String backwards = reverse(word);
-  if(sWord.equals(backwards))
+  if(word.equals(backwards))
     return true;
   return false;
 }

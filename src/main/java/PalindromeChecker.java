10052c3eb9e9319a6d3326895b442511c26d42a0
jachen28 <142927320+jachen28@users.noreply.github.com>
2024-01-31 09:14:48 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -44,9 +44,16 @@ public boolean palindrome(String word)
 }
 public String reverse(String str)
 {
+    
     String sNew = new String();
-  for(int i = str.length(); i > 0; i--)
+  for(int i = str.length(); i > 0; i--){
   sNew = sNew + str.substring(i-1,i);
+  if(!str.substring(i-1, i).equals(" "))
+  sNew = sNew + str.substring(i-1, i);
+    sNew.toLowerCase();
+  }
     return sNew;
 }
 }
+
+

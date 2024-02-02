8b1644150e026238585e8bae214e4220b21e4513
jachen28 <142927320+jachen28@users.noreply.github.com>
2024-02-01 20:04:35 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,7 +37,9 @@ public void tester()
 }
 public boolean palindrome(String word)
   {
- if(noPunctuation(onlyLetter(noSpaces(word.toLowerCase()))).equals(noSpaces(reverse(noPunctuation(onlyLetter(word.toLowerCase()))))) == true)
+    String backwards = reverse(word);
+  if(word.equals(backwards))
+ if(noPunctuation(onlyLetter(noSpaces(word.toLowerCase()))).equals(noSpaces(reverse(noPunctuation(onlyLetter(word.toLowerCase()))))) == true && word.equals(backwards))
     return true;
   return false;
 }
